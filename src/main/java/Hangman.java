2ae24400bea6f5be6b92a9cea1f8c8cbfbe184d5
bryonyoni <bryonyoni@gmail.com>
2017-04-21 12:38:42 +0300

    Refactored by changing string into an array
    changed the string containing the words to an array.

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,9 +1,11 @@
 public class Hangman{
 
- String listOfWords= "pancakes" ;
+ String[] listOfWords= {"pancakes","potatoes"} ;
+
+ Integer stringNumberInArray= 0;
 
  public boolean HangmanLetterToBeChecked(String character){
-   String WordToBeChecked =  listOfWords;
+   String WordToBeChecked =  listOfWords[stringNumberInArray];
     if(WordToBeChecked.contains(character)){
       return true;
     }else{
